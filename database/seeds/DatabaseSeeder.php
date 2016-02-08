9ef880df2bf29e9e7146145e7aaef30a544af2f4
Zachary Bruno <zachcbruno@gmail.com>
2016-02-07 16:51:43 -0800

    update master seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Rental;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        Rental::truncate();
+        $this->call('RentalsSeeder');
     }
 }
