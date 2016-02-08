12973e6c74a5b2fe1bcba89df9ae9a765255d3a2
Zachary Bruno <zachcbruno@gmail.com>
2016-02-07 20:22:45 -0800

    remove old templates

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,14 +5,4 @@
 @stop
 
 @section('content')
-    <div class="container">
-        <div class="content">
-            <div class="title">Laravel 5</div>
-            <ul>
-                @foreach ($people as $person)
-                    <li>{{$person}}</li>
-                @endforeach
-            </ul>
-        </div>
-    </div>
 @stop
